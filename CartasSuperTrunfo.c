4bdec5e2c2c8fbca5f07b2290442c2f0806504ea
Viviane <61329986+vivianesilvasouza@users.noreply.github.com>
2026-02-17 13:31:45 +0000

    refactor: organiza saída de dados e limpa buffer do teclado

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -1,80 +1,80 @@
 #include <stdio.h>
 
-int main (){
-    // Declaração de variáveis para armazenar dados do estado e da cidade.
-    // strings (arrays de char) para nomes e código, inteiros para população
-    // e pontos turísticos, e floats para área e PIB.
+int main() {
+    // --- CARTA 1 ---
     char nomeEstado[50], codidoCarta[5], NomeCidade[50];
     int populacao, numPontosTuristicos;
     float area, pib;
+    float densidade1, pibPerCapita1;
 
-    //exibe o identificado da carta 
-    printf("Carta 1: \n");
-
-    //leitura dos dados 
-    printf("Digite o nome do Estado: \n");
+    printf("--- Cadastro da Carta 1 ---\n");
+    printf("Digite o nome do Estado: ");
     scanf(" %[^\n]", nomeEstado);
-
-    printf("Digite o codigo da carta: \n");
+    printf("Digite o codigo da carta: ");
     scanf(" %[^\n]", codidoCarta);
-
-    printf("Digite o nome da Cidade: \n");
+    printf("Digite o nome da Cidade: ");
     scanf(" %[^\n]", NomeCidade);
-
-    printf("Digite a populacao: \n");
+    printf("Digite a populacao: ");
     scanf("%d", &populacao);
-    while (getchar() != '\n');
-
-    printf("Digite a area: \n");
+    printf("Digite a area: ");
     scanf("%f", &area);
-    while (getchar() != '\n');
-
-    printf("Digite o PIB: \n");
+    printf("Digite o PIB: ");
     scanf("%f", &pib);
-    while (getchar() != '\n');
-
-    printf("Digite o numero de pontos turisticos: \n");
+    printf("Digite o numero de pontos turisticos: ");
     scanf("%d", &numPontosTuristicos);
-    while (getchar() != '\n');
 
-    //este bloco por não criar um metodo, eu precisei repetir o código 
-    // igual de cima, a única diferença é os nomes das variaveis que precisei mudar
-    //pois dar erro por ser os mesmos nomes nas variaveis. 
+    // Cálculos da Carta 1
+    densidade1 = (float)populacao / area;
+    pibPerCapita1 = pib / (float)populacao;
+
+    // --- CARTA 2 ---
     char nomeEstado2[50], codidoCarta2[5], NomeCidade2[50];
     int populacao2, numPontosTuristicos2;
     float area2, pib2;
+    float densidade2, pibPerCapita2;
 
-    printf("Carta 2: \n");
-
-    printf("Digite o nome do Estado: \n");
+    printf("\n--- Cadastro da Carta 2 ---\n");
+    printf("Digite o nome do Estado: ");
     scanf(" %[^\n]", nomeEstado2);
-
-    printf("Digite o codigo da carta: \n");
+    printf("Digite o codigo da carta: ");
     scanf(" %[^\n]", codidoCarta2);
-
-    printf("Digite o nome da Cidade: \n");
+    printf("Digite o nome da Cidade: ");
     scanf(" %[^\n]", NomeCidade2);
-
-    printf("Digite a populacao: \n");
+    printf("Digite a populacao: ");
     scanf("%d", &populacao2);
-    while (getchar() != '\n');
-
-    printf("Digite a area: \n");
+    printf("Digite a area: ");
     scanf("%f", &area2);
-    while (getchar() != '\n');
-
-    printf("Digite o PIB: \n");
+    printf("Digite o PIB: ");
     scanf("%f", &pib2);
-    while (getchar() != '\n');
-
-    printf("Digite o numero de pontos turisticos: \n");
+    printf("Digite o numero de pontos turisticos: ");
     scanf("%d", &numPontosTuristicos2);
-    while (getchar() != '\n');
 
-    printf("\ncarta 1 \n Estado: %s \n Codigo da carta: %s \n Cidade: %s \n Populacao: %d \n Area: %.2f \n PIB: %.2f \n Pontos Turisticos: %d \n",
-           nomeEstado, codidoCarta, NomeCidade, populacao, area, pib, numPontosTuristicos);
+    // Cálculos da Carta 2
+    densidade2 = (float)populacao2 / area2;
+    pibPerCapita2 = pib2 / (float)populacao2;
 
-    printf("carta 2 \n Estado: %s \n Codigo da carta: %s \n Cidade: %s \n Populacao: %d \n Area: %.2f \n PIB: %.2f \n Pontos Turisticos: %d \n",
-           nomeEstado2, codidoCarta2, NomeCidade2, populacao2, area2, pib2, numPontosTuristicos2);
+    // --- EXIBIÇÃO DOS DADOS ---
+    printf("\n==============================\n");
+    printf("CARTA 1: %s (%s)\n", NomeCidade, nomeEstado);
+    printf("Densidade: %.2f hab/km² | PIB per Capita: %.2f\n", densidade1, pibPerCapita1);
+    
+    printf("CARTA 2: %s (%s)\n", NomeCidade2, nomeEstado2);
+    printf("Densidade: %.2f hab/km² | PIB per Capita: %.2f\n", densidade2, pibPerCapita2);
+    printf("==============================\n");
 
+    // --- LÓGICA DE COMPARAÇÃO ---
+    // Atributo escolhido: População
+    printf("\nComparação de cartas (Atributo: População):\n\n");
+    printf("Carta 1 - %s: %d\n", NomeCidade, populacao);
+    printf("Carta 2 - %s: %d\n", NomeCidade2, populacao2);
+
+    if (populacao > populacao2) {
+        printf("Resultado: Carta 1 (%s) venceu!\n", NomeCidade);
+    } else if (populacao2 > populacao) {
+        printf("Resultado: Carta 2 (%s) venceu!\n", NomeCidade2);
+    } else {
+        printf("Resultado: Empate!\n");
+    }
+
+    return 0;
 }
